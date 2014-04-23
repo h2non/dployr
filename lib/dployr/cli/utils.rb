55e83acbbe6b3e3ed91772008704ed8eb3adc8b6
Tomas Aparicio <tomas@aparicio.me>
2014-04-23 11:46:42 +0200

    feat(cli): add utils module in CLI

---
+++ lib/dployr/cli/utils.rb
@@ -0,0 +1,23 @@
+require 'dployr/utils'
+
+module Dployr
+  module CLI
+    module Utils
+
+      include Dployr::Utils
+
+      module_function
+
+      def parse_attributes(attributes)
+        if attributes.is_a? String
+          if @options[:attributes][0] == '-'
+            parse_flags attributes
+          else
+            parse_matrix attributes
+          end
+        end
+      end
+
+    end
+  end
+end
---
+++ lib/dployr/compute/aws.rb
@@ -0,0 +1,28 @@
+require 'fog'
+
+module Dployr
+  module Compute
+    class AWS
+
+      attr_reader :client
+
+      DEFAULT = {
+        provider: 'AWS'
+      }
+
+      def initialize(options)
+        @options = options
+        @client = Fog::Compute.new DEFAULT.merge options
+      end
+
+      def servers
+        @client.servers
+      end
+
+      def create_server(options)
+        @client.servers.create options
+      end
+
+    end
+  end
+end
