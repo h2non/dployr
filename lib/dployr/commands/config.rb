36261046da54bae289715a0ffdb96d0b0ecf1899
Tomas Aparicio <tomas@aparicio.me>
2014-04-30 00:32:23 +0200

    refactor(config): minor refactor and config command improvments

--- lib/dployr/commands/config.rb
+++ lib/dployr/commands/config.rb
@@ -15,25 +15,32 @@ module Dployr
         end
       end
 
+      private
+
       def render_file
         raise "Dployrfile was not found" if @dployr.file_path.nil?
         raise "Configuration is missing" unless @dployr.config.exists?
+
         begin
-          if @name and options[:provider] and options[:region]
-            config = get_region_config options
-          elsif @name
-            config = @dployr.config.get_config @name, @attrs
-          else
-            config = @dployr.config.get_config_all @attrs
-          end
-          unless config.nil?
-            puts config.to_yaml
-          else
-            raise "Missing configuration data"
-          end
+          print_config
         rescue Exception => e
           puts "Cannot generate the config: #{e}"
           exit 1
+        end
+      end
+
+      def print_config
+        if @name and @options[:provider] and @options[:region]
+          config = get_region_config @options
+        elsif @name
+          config = @dployr.config.get_config @name, @attrs
+        else
+          config = @dployr.config.get_config_all @attrs
+        end
+        unless config.nil?
+          puts config.to_yaml
+        else
+          raise "Missing configuration data"
         end
       end
 
